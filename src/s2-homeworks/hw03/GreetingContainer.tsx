db20365b4399b99ee7186208dcc6ca4ac62582b2
Heavyvetal9 <buryanov9@yandex.ru>
2024-04-11 22:02:40 +0300

    2-sprint-2week-01

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -3,36 +3,41 @@ import Greeting from './Greeting'
 import { UserType } from './HW3'
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // fixed any
+    addUserCallback: (name: string) => void // fixed any
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
-    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+export const pureAddUser = (name: string, setError: (error: string | null) => void, setName: (name: string) => void, addUserCallback: (name: string) => void) => {
+    if (name.trim() === '') {
+        setError('Name is required')
+    } else {
+        addUserCallback(name)
+        setName('')
+    }
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: (error: string | null) => void) => {
+    if (name.trim() === '') {
+        setError('Name is required')
+    }
 }
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => {
+    if (e.key === 'Enter') {
+        addUser()
+    }
 }
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
-
-// более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
-    // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
+    const [name, setName] = useState<string>('') // fixed any
+    const [error, setError] = useState<string | null>(null) // fixed any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
-
-        error && setError('')
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value) // fixed
+        error && setError(null)
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
@@ -42,12 +47,12 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnBlur(name, setError)
     }
 
-    const onEnter = (e: any) => {
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length // fixed
+    const lastUserName = users[users.length - 1]?.name || '' // fixed
 
     return (
         <Greeting
--- src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
+++ src/s2-homeworks/hw03/tests/pureAddUserCallback.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {pureAddUserCallback} from '../HW3'
+import { pureAddUserCallback } from '../HW3'
 
-let initialState: any[]
-const setName = (a: any[]) => {
-    initialState = a
-}
+let initialState: Array<{name: string, _id: string}> = [];
+const setName = (a: Array<{name: string, _id: string}>) => {
+    initialState = [...a];
+};
 
 beforeEach(() => {
-    initialState = []
-})
+    initialState = [];
+});
 
 test('name 1', () => {
-    pureAddUserCallback('name', setName, initialState)
-    expect(initialState.length).toBe(1)
-    expect(initialState[0].name).toBe('name')
-    expect(!!initialState[0]._id).toBe(true)
-})
+    pureAddUserCallback('name', setName, initialState);
+    expect(initialState.length).toBe(1);
+    expect(initialState[0].name).toBe('name');
+    expect(typeof initialState[0]._id).toBe('string');
+});
