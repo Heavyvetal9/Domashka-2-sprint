916a9621b6ffb47111789657a84c74e8d5032872
Heavyvetal9 <buryanov9@yandex.ru>
2024-07-30 17:10:47 +0300

    3-sprint-2week-02

--- src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
+++ src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
@@ -21,7 +21,17 @@ test('sort name up', () => {
         payload: 'up',
     })
 
-    expect(newState[0]._id).toBe(1)
+    // expect(newState[0]._id).toBe(1)
+
+    const result = [
+        1,
+        3,
+        4,
+        5,
+        2,
+        0
+    ].filter((el, i) => el === newState[i]._id)
+    expect(result.length).toBe((newState.length))
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {
